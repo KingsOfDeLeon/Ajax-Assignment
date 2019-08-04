74514287c3469226034010fd877db5dce1b774b2
Luke DeLeon <lukedeleon94@gmail.com>
2019-08-03 20:21:12 -0700

    Created Clear button functionality, added weight to label text, modified readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Ajax-Assignment
-AJAX practice with the Giphy API
+AJAX practice with the Giphy API.
+
+
+Search up some gifs within the text field.
+This will create additional buttons to display (A few have already been generated for you!)
+Press a button and this will display a few gifs along with their rating!
+Click on a gif to have it start moving, click it again to pause it.
+
+
+Press on the clear button to remove all gifs/buttons. 
--- assets/javascript/appLogic.js
+++ assets/javascript/appLogic.js
@@ -1,14 +1,16 @@
 $(document).ready(function () {
+    //Array to populate sample gif buttons
     var gifArray = ['dog', 'cat', 'rabbit', 'hamster', 'skunk'];
 
 
+    // function used to display gifs and gif ratings
     function displayGifInfo() {
         $(".gifs-Go-Here").empty();
         var gif = $(this).attr("data-name");
 
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=HxNN8PMYFlAzMfWLx1MU6fWhETsHfIrb&q=" + gif + "&limit=10";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=HxNN8PMYFlAzMfWLx1MU6fWhETsHfIrb&q=" + gif + "&limit=20";
 
-
+        //AJAX call + img and div creation for displaying gifs and gif ratings
         $.ajax({
                 url: queryURL,
                 method: "GET"
@@ -23,7 +25,12 @@ $(document).ready(function () {
                     gif.addClass("float-sm-left m-1");
                     var gifRating = $("<div>").html("Rated: " + results[i].rating);
                     var gifImage = $("<img>");
-                    gifImage.attr("src", results[i].images.fixed_height_small.url);
+                    gifImage.addClass("gif-click");
+                    gifImage.attr("src", results[i].images.fixed_height_small_still.url);
+                    gifImage.attr("data-still", results[i].images.fixed_height_small_still.url);
+                    gifImage.attr("data-animate", results[i].images.fixed_height_small.url);
+                    gifImage.attr("data-state", "still");
+                    //console.log("Gif attributes: " + gifImage.attr("src"));
                     $(gif).append(gifRating);
                     $(gif).append(gifImage);
                     $(".gifs-Go-Here").append(gif);
@@ -33,31 +40,60 @@ $(document).ready(function () {
     };
 
 
-
+//function used to create gif generation buttons
     function renderButtons() {
         $(".buttons-Go-Here").empty();
         for (var i = 0; i < gifArray.length; i++) {
             console.log("Making buttons for " + gifArray[i]);
             var a = $("<button>");
-            a.addClass("gif btn btn-primary mx-1");
+            a.addClass("gif btn btn-primary mx-1 mb-1");
             a.attr("data-name", gifArray[i]);
             a.text(gifArray[i]);
             $(".buttons-Go-Here").append(a);
         }
     };
 
+//removes all buttons
+    function clear() {
+        event.preventDefault();
+        $(".buttons-Go-Here").empty();
+        $(".gifs-Go-Here").empty();
+        gifArray = [];
+    };
+
+// grabs text from as search criteria for giphy API. If no text provided, does nothing.
     $(".add-gif").on("click", function (event) {
         event.preventDefault();
         var gif = "";
         gif = $("#gif-Search").val().trim();
-        console.log("inserting: " + gif + " into the Array");
+        if (gif === "") {
+            console.log("No empty submissions");
+            return false;
+        };
+        //console.log("inserting: " + gif + " into the Array");
         gifArray.push(gif);
-        console.log("Array is now " + gifArray);
+        //console.log("Array is now " + gifArray);
         renderButtons();
     });
 
-
-    $(document).on("click", ".gif", displayGifInfo)
+// click event that animates or removes animation from generated gifs
+    $(document).on("click", ".gif-click", function () {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            //console.log("gif was still");
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            //console.log("gif was moving");
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
+//when clear button is pressed, call clear function
+    $(".clear").on("click", clear);
+//when a gif button is pressed, generate associated gifs
+    $(document).on("click", ".gif", displayGifInfo);
+//generates button
     renderButtons();
 
 });
